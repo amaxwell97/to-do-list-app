dcf015687984ff6a5095fa77639fc669e455cd3e
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-04 21:09:23 -0600

    Declare addButton, give EventListener, add if check

---
+++ app.js
@@ -0,0 +1,13 @@
+const addButton = document.querySelector(#add-button);
+
+addButton.addEventListener('click', () => {
+    let newTask = document.querySelector(#add-task);
+    const errorMessage = document.querySelector(#error-message);
+
+    if (newTask == '') {
+        errorMessage.style.display = 'block';
+        return;
+    } else {
+        
+    }
+})
