56d6095b041d93d9e7c3bdd299860f429478c880
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-04 23:29:12 -0600

    Give addTask argument, set newListItem to newTask

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const addButton = document.querySelector('#add-button');
+let newTask = document.querySelector('#add-task');
+const addButton = document.querySelector('#submit-button');
 const unorderedList = document.querySelector('#unordered-list');
+const taskArray = [];
 
-const addTask = () => {
-    let newTask = document.querySelector('#add-task');
+const addTask = (newTask) => {
     const errorMessage = document.querySelector('#error-message');
+    const newListItem = document.createElement('li');
 
     if (newTask == '') {
         errorMessage.style.display = 'block';
     } else {
-
+        newListItem.textContent = newTask;
+        console.log(newListItem);
+        unorderedList.appendChild(newListItem);
     }
 }
 
 addButton.addEventListener('click', () => {
-    
+    addTask(newTask.value);
 })
