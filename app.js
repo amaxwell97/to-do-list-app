c0d075cb52befafe2976e157fef23c90825c6a7f
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-08 22:13:45 -0600

    Push task to array. Declare checkOffTask function

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const taskArray = [];
 const addTask = (newTask) => {
     const errorMessage = document.querySelector('#error-message');
     const newListItem = document.createElement('li');
-    const newListItemCheck = document.createElement('input');
+    const newListItemCheck = document.createElement('img');
     const newListItemPara = document.createElement('p');
     
-    newListItemCheck.type = 'checkbox';
+    newListItemCheck.src = 'to-do-list-app-images/unchecked.png';
 
     if (newTask == '') {
         errorMessage.style.display = 'block';
@@ -18,9 +18,27 @@ const addTask = (newTask) => {
         newListItem.appendChild(newListItemCheck);
         newListItem.appendChild(newListItemPara)
         newListItemPara.textContent = newTask;
+        taskArray.push({
+            itemCheck: newListItemCheck.src,
+            newTask: newTask,
+        });
+        console.log(taskArray);
     }
 }
+
+// const checkOffTask = () => {
+//     taskArray.forEach((task) => {
+//         task.addEventListener('click', () {
+//             task.itemCheck.src
+//         })
+//     })
+// }
 
 addButton.addEventListener('click', () => {
     addTask(newTask.value);
 })
+
+// unorderedList.addEventListener = ('click', () => {
+//     checkOffTask();
+// })
+
