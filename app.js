574525bda47e45c4d8ee54fbdefb4d5096b0b491
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-10 15:21:21 -0600

    Move newTask variable within addTask code block

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const newTask = document.querySelector('#add-task');
 const addButton = document.querySelector('#submit-button');
 const unorderedList = document.querySelector('#unordered-list');
 
 const addTask = () => {
+    const newTask = document.querySelector('#add-task');
     const errorMessage = document.querySelector('#error-message');
     const newListItem = document.createElement('li');
 
