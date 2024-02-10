60d0b52eb05061e7c7a0e6806024902cbbbef26f
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-10 15:13:15 -0600

    Remove checkOffTask and list item img, p variables

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
-let newTask = document.querySelector('#add-task');
+const newTask = document.querySelector('#add-task');
 const addButton = document.querySelector('#submit-button');
 const unorderedList = document.querySelector('#unordered-list');
-const taskArray = [];
 
-const addTask = (newTask) => {
+const addTask = () => {
     const errorMessage = document.querySelector('#error-message');
     const newListItem = document.createElement('li');
-    const newListItemCheck = document.createElement('img');
-    const newListItemPara = document.createElement('p');
-    
-    newListItemCheck.src = 'to-do-list-app-images/unchecked.png';
 
-    if (newTask == '') {
+    if (newTask.value == '') {
         errorMessage.style.display = 'block';
     } else {
+        newListItem.innerText = newTask.value;
         unorderedList.appendChild(newListItem);
-        newListItem.appendChild(newListItemCheck);
-        newListItem.appendChild(newListItemPara)
-        newListItemPara.textContent = newTask;
-        taskArray.push({
-            itemCheck: newListItemCheck.src,
-            newTask: newTask,
-        });
-        console.log(taskArray);
+        errorMessage.style.display = 'none'
     }
 }
 
-const checkOffTask = () => {
-    taskArray.forEach((task) => {
-        task.addEventListener('click', () => {
-            task.itemCheck = 'http://127.0.0.1:5500/to-do-list-app-images/checked.png';
-        })
-    })
-}
-
 addButton.addEventListener('click', () => {
-    addTask(newTask.value);
+    addTask();
 })
-
-// unorderedList.addEventListener = ('click', () => {
-//     checkOffTask();
-// })
-
