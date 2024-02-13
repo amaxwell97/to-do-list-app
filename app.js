e2f0c6a48a8cdcfcc2e17ea5bbf1cfc049549309
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-12 21:15:19 -0600

    Declare span in addTask, add to newListItem

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const addTask = () => {
     const newTask = document.querySelector('#add-task');
     const errorMessage = document.querySelector('#error-message');
     const newListItem = document.createElement('li');
+    const span = '\u2717';
 
     if (newTask.value == '') {
         errorMessage.style.display = 'block';
     } else {
-        newListItem.innerText = newTask.value;
+        newListItem.innerText = `${newTask.value} ${span}`;
         unorderedList.appendChild(newListItem);
-        errorMessage.style.display = 'none'
+        errorMessage.style.display = 'none';
+        newTask.value = '';
     }
 }
 
